acf172615b6fcc828fc1f2b52d8252bb6a4fbd41
Anshumaan Singh <anshumaanborntolead@gmail.com>
2023-12-20 11:48:37 +0530

    add

--- Arrays/find_the_union_and_intersection_of_the_two_sorted_arrays.java
+++ Arrays/find_the_union_and_intersection_of_the_two_sorted_arrays.java
@@ -1,4 +1,51 @@
 package Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
 public class find_the_union_and_intersection_of_the_two_sorted_arrays {
-    
+    public static ArrayList<Integer> findSimilarity(ArrayList<Integer> arr1, ArrayList<Integer> arr2, int n, int m) {
+        ArrayList<Integer> result=new ArrayList<>();
+        HashSet<Integer> union=new HashSet<>();
+        HashSet<Integer> intersection=new HashSet<>();
+        if(n>m){
+            for(int i=0;i<m;i++){
+                if(arr1.contains(arr2.get(i))){
+                    intersection.add(arr2.get(i));
+                }
+            }
+        }
+        else if(m>n){
+            for(int i=0;i<n;i++){
+                if(arr2.contains(arr1.get(i))){
+                    intersection.add(arr1.get(i));
+                }
+            }
+        }
+        int intersection_size=intersection.size();
+        result.add(intersection_size);
+        for(int i=0;i<n;i++){
+            union.add(arr1.get(i));
+        }
+        for(int i=0;i<m;i++){
+            union.add(arr2.get(i));
+        }
+        int union_size=union.size();
+        result.add(union_size);
+        return result;
+	}
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int M=sc.nextInt();
+        ArrayList<Integer> ARR1=new ArrayList<>();
+        ArrayList<Integer> ARR2=new ArrayList<>();
+        for(int i=0;i<N;i++){
+            ARR1.add(sc.nextInt());
+        }
+        for(int i=0;i<M;i++){
+            ARR2.add(sc.nextInt());
+        }
+        ArrayList<Integer> response=findSimilarity(ARR1, ARR2, N, M);
+        System.out.println(response);
+    }
 }
---
+++ Arrays/sort_0_1_2.java
@@ -0,0 +1,46 @@
+//Accepted
+package Arrays;
+import java.util.Scanner;
+public class sort_0_1_2 {
+    public static void sort012(int[] arr)
+    {
+        int n=arr.length;
+        int c0=0,c1=0,c2=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                c0++;
+            }
+            else if(arr[i]==1){
+                c1++;
+            }
+            else if(arr[i]==2){
+                c2++;
+            }
+        }
+        int i=0;
+        while (i<n) {
+            if(i<c0){
+                arr[i]=0;
+            }
+            else if(i>=c0 && i<c0+c1){
+                arr[i]=1;
+            }
+            else if(i>=c0+c1 && i<c0+c1+c2){
+                arr[i]=2;
+            }
+            i++;  
+        }
+        for(int x:arr){
+            System.out.println(x);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int ARR[]=new int[N];
+        for(int i=0;i<N;i++){
+            ARR[i]=sc.nextInt();
+        }
+        sort012(ARR);
+    }
+}
